ca7979ff0ce12fde1dab988c94cc0f4e0daed0db
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-11-17 12:47:29 +0100

    Intentar usar resulset para ahcer CRUD de persona

--- src/capaDAL/BoletoCRUD.java
+++ src/capaDAL/BoletoCRUD.java
@@ -1,9 +1,25 @@
 package capaDAL;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import modelo.Boleto;
 
 public class BoletoCRUD {
-	public boolean insertaBoleto(Boleto boleto){
-		DbConnexion miConexion=new DbConnexion();
+	public int insertaBoleto(Boleto boleto){
+		DbConnexion miConexion=DbConnexion.getDbConnexion();
+		
+		Statement instruccion;
+		ResultSet resultado;
+		
+		try {
+			instruccion=miConexion.getConexionBasedeDatos().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		
+		
 	}
 }
